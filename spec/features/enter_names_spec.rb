f9bdf037d9a9b7246458d3c9df7e3a8c0c5259b1
johnnydee8 <j.dworzynski@me.com>
2016-08-31 18:23:53 +0100

    21 - web helpers extraction

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,20 +2,14 @@ require 'spec_helper'
 
 feature 'entering names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in(:player_1_name, with: 'Jonny')
-    fill_in(:player_2_name, with: 'Ben')
-    click_button('Play')
+    sign_in_and_play
     expect(page).to have_content('Jonny vs. Ben')
   end
 end
 
 feature 'Displaying hitpoints' do
   scenario 'Player 1 can see Player 2 hitpoints' do
-    visit('/')
-    fill_in(:player_1_name, with: 'Jonny')
-    fill_in(:player_2_name, with: 'Ben')
-    click_button('Play')
+    sign_in_and_play
     expect(page).to have_content('45/60 HP')
   end
 end
