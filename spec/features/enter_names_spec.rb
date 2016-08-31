751706f5ae3a1a2d2cfbc7d5b46df7dc3d768a76
BenRoss92 <benjaminjackross@gmail.com>
2016-08-31 16:28:11 +0100

    displays player 2's hitpoints

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,3 +9,13 @@ feature 'entering names' do
     expect(page).to have_content('Jonny vs. Ben')
   end
 end
+
+feature 'Displaying hitpoints' do
+  scenario 'Player 1 can see Player 2 hitpoints' do
+    visit('/')
+    fill_in(:player_1_name, with: 'Jonny')
+    fill_in(:player_2_name, with: 'Ben')
+    click_button('Play')
+    expect(page).to have_content('45/60 HP')
+  end
+end
